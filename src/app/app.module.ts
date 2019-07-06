7e4a777eae0673e714090c94a715e0f2ad5e11c7
SNiemiec <slawekniemiec@gmail.com>
2019-07-07 01:57:24 +0200

    Fixed angular/core7 v material8 issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {SidenavDrawerOverviewExample} from './sidenav-drawer-overview-example/sidenav-drawer-overview-example';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavDrawerOverviewExample
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidenav-drawer-overview-example/sidenav-drawer-overview-example.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'sidenav-drawer-overview-example',
+  templateUrl: './sidenav-drawer-overview-example.html',
+  styleUrls: ['./sidenav-drawer-overview-example.css'],
+})
+export class SidenavDrawerOverviewExample {}
